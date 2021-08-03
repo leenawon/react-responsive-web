e9558317dc34019a61b5b2d763074b4d172b29b2
leenawon <nawon5154@gmail.com>
2021-08-04 01:56:51 +0900

    Navbar.js에 Button component 추가

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,29 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from './Button';
+import './Navbar.css';
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
   const handleMenuClick = () => setClick(!click);
   const closeNavMenu = () => setClick(false);
+  const displayButton = () => {
+    return (window.innerWidth <= 960) ? setButton(false) : setButton(true);
+  }
+
+  useEffect(() => {
+    displayButton();
+  }, []);
+
+  window.addEventListener('resize', displayButton);
+
   return (
     <>
       <nav className="navbar">
         <div className="nav-container">
           {/* Nav Bar Logo */}
-          <Link to="/" className="nav-logo">
+          <Link to="/" className="nav-logo" onClick={closeNavMenu}>
             <i className="fas fa-quote-left"></i>
             Nature
             <i className="fas fa-quote-right"></i>
@@ -19,7 +32,9 @@ function Navbar() {
           <div className="nav-menu-icon" onClick={handleMenuClick}>
             <i className={click ? "fas fa-minus-circle" : "fas fa-chevron-circle-down"}></i>
           </div>
+          {/* Nav Menu List */}
           <ul className={click ? "nav-menu-list active" : "nav-menu-list"}>
+            {/* Nav Menu Item */}
             <li className="nav-menu-item">
               <Link to="/" className="menu-links" onClick={closeNavMenu}>
                 Home
@@ -36,11 +51,13 @@ function Navbar() {
               </Link>
             </li>
             <li className="nav-menu-item">
-              <Link to="/signup" className="menu-links" onClick={closeNavMenu}>
+              <Link to="/signup" className="sign-up-links" onClick={closeNavMenu}>
                 Sign up
               </Link>
             </li>
           </ul>
+          {/* Button Component */}
+          {button && <Button button_style="outline-button">Sign up</Button>}
         </div>
       </nav>
     </>
