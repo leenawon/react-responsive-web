914b2dc2f0517355b350d094fe35c3ac56870b2f
leenawon <nawon5154@gmail.com>
2021-08-06 21:56:42 +0900

    Hero Image component 추가

---
+++ src/components/HeroImage.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Button } from './Button';
+import './HeroImage.css';
+import '../App.css';
+import { Link } from 'react-router-dom';
+
+function HeroImage() {
+  return (
+    <div className="hero">
+      <h1>Green Space</h1>
+      <p>The calming power of nature</p>
+      {/* Hero Button */}
+      <div className="hero-buttons">
+        {/* Button Component */}
+        <Link to ="/articles" className="button">
+          <Button className="hero-button" button_style="outline-button" button_size="large-button">
+              Calming Place <i className="fas fa-arrow-circle-right"></i>
+          </Button>
+        </Link>
+        <Link to="/services" className="button">
+          <Button className="hero-button" button_style="basic-button" button_size="large-button">
+              Experience
+          </Button>
+        </Link>
+        
+      </div>
+    </div>
+  )
+}
+
+export default HeroImage;
