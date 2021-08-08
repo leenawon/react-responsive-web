cf113190bfc0777b183c8711cdc753bdd15b112b
leenawon <nawon5154@gmail.com>
2021-08-09 00:49:52 +0900

    HomPage Component 추가 (pages 폴더)

---
+++ src/components/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../App.css';
+import HeroImage from '../HeroImage';
+
+function HomePage() {
+  return (
+    <div>
+      {/* HeroImage Component */}
+      <HeroImage></HeroImage>
+    </div>
+  )
+}
+
+export default HomePage;
