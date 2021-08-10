f7198196f2900d19d3a7fa22c3c8718c64917d6a
leenawon <nawon5154@gmail.com>
2021-08-11 02:40:52 +0900

    HomePage.js에 ImageCard component 추가

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import '../../App.css';
 import HeroImage from '../HeroImage';
+import ImageCard from '../ImageCard';
 
 function HomePage() {
   return (
     <div>
       {/* HeroImage Component */}
       <HeroImage></HeroImage>
+      {/* Image Card Component */}
+      <ImageCard></ImageCard>
     </div>
   )
 }
