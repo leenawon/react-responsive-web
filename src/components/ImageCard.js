51039b019b7c0869cc67ef3bb4f06d91267efcea
leenawon <nawon5154@gmail.com>
2021-08-12 01:25:53 +0900

    ImageCard.js CardItem 이미지 경로 변경

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import CardItem from './CardItem';
 import './ImageCard.css';
+import cozy from '../images/nature-image-4.jpg';
+import calming from '../images/nature-image-5.jpg';
+import comfortable from '../images/nature-image-6.jpg';
 
 function ImageCard() {
   return (
@@ -13,9 +16,9 @@ function ImageCard() {
           {/* Card List */}
           <ul className="card-list">
             {/* CardItem Component */}
-            <CardItem src="https://cdn.pixabay.com/photo/2017/09/16/16/08/lake-2755907__340.jpg" text="We admire the beauty of nature" category="calming" path="/articles"></CardItem>
-            <CardItem src="https://cdn.pixabay.com/photo/2018/08/21/23/29/forest-3622519__340.jpg" text="Slow down and enjoy the simple pleasures in life" category="cozy" path="/articles"></CardItem>
-            <CardItem src="https://cdn.pixabay.com/photo/2019/07/14/10/48/river-4336788__340.jpg" text="Nature is not a place to visit, It is home" category="comfortable" path="/articles"></CardItem>
+            <CardItem src={cozy} text="We admire the beauty of nature" category="cozy" path="/articles"></CardItem>
+            <CardItem src={calming} text="Slow down and enjoy the simple pleasures in life" category="calming" path="/articles"></CardItem>
+            <CardItem src={comfortable} text="Nature is not a place to visit, It is home" category="comfortable" path="/articles"></CardItem>
           </ul>
         </div>
       </div>
