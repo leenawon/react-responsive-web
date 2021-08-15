2e93cc5dd2080f7f52b029634ae1e428678404dd
leenawon <nawon5154@gmail.com>
2021-08-15 21:06:03 +0900

    Article.js page component 추가

---
+++ src/components/pages/Article.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../App.css';
+
+function Article() {
+  return (
+    <div>
+      <h1 className="articles">Articles</h1>
+    </div>
+  )
+}
+
+export default Article;
