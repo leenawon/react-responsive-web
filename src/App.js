515c2051642fb61d9c9287cc2972ac17591f88da
leenawon <nawon5154@gmail.com>
2021-08-29 02:16:48 +0900

    App.js에 Article, Service, SignUp component 추가

--- src/App.js
+++ src/App.js
@@ -3,14 +3,25 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import HomePage from './components/pages/HomePage';
+import Article from './components/pages/Article';
+import Service from './components/pages/Service';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+      {/* NavBar Component */}
         <Navbar></Navbar>
         <Switch>
+          {/* HomePage Component */}
           <Route exact path="/" component={HomePage}></Route>
+          {/* Article Component */}
+          <Route exact path="/articles" component={Article}></Route>
+          {/* Service Component */}
+          <Route exact path="/services" component={Service}></Route>
+          {/* Sign Up Component */}
+          <Route exact path="/signup" component={SignUp}></Route>
         </Switch>
       </BrowserRouter>
     </>
