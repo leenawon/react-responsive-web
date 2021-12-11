5f64ba7a001b276000905db66e6930a01bdf2c72
leenawon <nawon5154@gmail.com>
2021-12-12 01:46:39 +0900

    package.json, App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import HomePage from './components/pages/HomePage';
@@ -10,20 +10,20 @@ import SignUp from './components/pages/SignUp';
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <Router>
       {/* NavBar Component */}
         <Navbar></Navbar>
         <Switch>
           {/* HomePage Component */}
           <Route exact path="/" component={HomePage}></Route>
           {/* Article Component */}
-          <Route exact path="/articles" component={Article}></Route>
+          <Route path="/articles" component={Article}></Route>
           {/* Service Component */}
-          <Route exact path="/services" component={Service}></Route>
+          <Route path="/services" component={Service}></Route>
           {/* Sign Up Component */}
-          <Route exact path="/signup" component={SignUp}></Route>
+          <Route path="/signup" component={SignUp}></Route>
         </Switch>
-      </BrowserRouter>
+      </Router>
     </>
   );
 }
